9d2027d4e1bc2c85673e3f7fbab824f559cbf18b
Alexander Sklar <asklar@microsoft.com>
2020-05-15 17:10:51 -0700

    make nuget

--- make_nuget.cmd
+++ make_nuget.cmd
@@ -1,3 +1,8 @@
 @echo off
-msbuild /restore %~dp0\src\TreeDumpLibrary\ /p:configuration=Release
-nuget pack %~dp0\src\TreeDumpLibrary
+set /p version=< %~dp0\LastVersion.txt
+echo Current version: %version%
+set /a newVersion=%version% + 1
+echo New version: %newVersion%
+nuget pack %~dp0\src\TreeDumpLibrary\TreeDumpLibrary.csproj -Build -Symbols -Properties Configuration=Release -Version 1.0.%newVersion%
+echo %newVersion% > %~dp0\LastVersion.txt
+git add %~dp0\LastVersion.txt
