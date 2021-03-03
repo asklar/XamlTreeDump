19509f74e9579fe3814620c435db00e43a66ad49
Alexander Sklar <asklar@microsoft.com>
2021-03-02 19:08:13 -0800

    try to get source embed in pdb to work

--- make_nuget.cmd
+++ make_nuget.cmd
@@ -3,6 +3,6 @@ set /p version=< %~dp0\LastVersion.txt
 echo Current version: %version%
 set /a newVersion=%version% + 1
 echo New version: %newVersion%
-nuget pack %~dp0\src\TreeDumpLibrary\TreeDumpLibrary.csproj -Build -Symbols -Properties Configuration=Release -Version 1.0.%newVersion%
+nuget pack %~dp0\src\TreeDumpLibrary\TreeDumpLibrary.csproj -Build -Properties Configuration=Debug -Version 1.0.%newVersion%
 echo %newVersion% > %~dp0\LastVersion.txt
 git add %~dp0\LastVersion.txt
